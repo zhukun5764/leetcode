f044b7ace5953c2b36214c653f426ddfdeb936bc
zhukun <zhukun5764@163.com>
2020-09-08 20:28:47 +0800

    leetcode_test1 commit1

--- src/leetcode/test.java
+++ src/leetcode/test.java
@@ -8,7 +8,7 @@ public class test {
 
     public static void main(String[] args) {
 
-
+        System.out.println(12312);
 //        TestAbstract testAbstract = new TestAbstractImpl();
 
         TestAbstractImpl.load();
